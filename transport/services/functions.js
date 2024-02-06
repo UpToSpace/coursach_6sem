44ab7eac6e9491729a90b5eefddef0310796c4f8
Korzhova Valeria <korzhovalera2002@gmail.com>
2024-02-06 19:06:51 +0300

    .

--- transport/services/functions.js
+++ transport/services/functions.js
@@ -4,7 +4,7 @@ function convertArrivalTimeToMilliseconds(time) {
 
     // Create a Date object with today's date and the specified hours and minutes
     const currentDate = new Date();
-    currentDate.setDate(currentDate.getDate() + 1);
+    currentDate.setDate(currentDate.getDate());
     currentDate.setHours(hours);
     currentDate.setMinutes(minutes);
 
